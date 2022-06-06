5f544f0a8987c36956577097444f74133d455fc8
Benjamin <shallifi@hotmail.com>
2022-06-05 23:02:03 -0400

    commit tree a4b4efd67d3b62f06af511a6ca7a01dc8e563800

--- control_flow.rb
+++ control_flow.rb
@@ -1,5 +1,9 @@
 def admin_login(username, password)
-  # your code here
+  if  (username == "admin" || username =="ADMIN") && password == "12345"
+    "Access granted"
+  else 
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
